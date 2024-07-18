3e19ef5b3bc1bcb50f9553188693f9dc54220b5b
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-07-18 16:47:30 -0600

    barebones test functionality

--- dotmgr/helper.py
+++ dotmgr/helper.py
@@ -1,19 +1,54 @@
 #!/usr/bin/env python3
 
 import fdtools
+import argparse
 
-
-# directory constants
-HOME = fdtools.home()
-
-# define the dotfiles directory based on the current working directory by default, but allow overrides.
-DOTFILES_DIR = fdtools.pwd() if (overrideDotfilesDir := input(f'Dotfiles directory: [{fdtools.pwd()}] ')) == '' else overrideDotfilesDir
-
-if not fdtools.isDir(DOTFILES_DIR):
-	print('Dotfiles folder path is not a directory.')
-	exit(1)
+# global HOME, DOTFILES_DIR
 
 
 
-print(HOME)
-print(DOTFILES_DIR)
+def sync(files: list[str] = []):
+	allFiles = fdtools.lsAll(DOTFILES_DIR)
+	print(allFiles.find('.bashrc'))
+
+
+
+def init():
+	global HOME, DOTFILES_DIR
+	# directory constants
+	HOME = fdtools.home()
+
+	# define the dotfiles directory based on the current working directory by default, but allow overrides.
+	DOTFILES_DIR = fdtools.pwd() if (overrideDotfilesDir := input(f'Dotfiles directory: [{fdtools.pwd()}] ')) == '' else overrideDotfilesDir
+
+	if not fdtools.isDir(DOTFILES_DIR):
+		print('Dotfiles folder path is not a directory.')
+		exit(1)
+
+
+
+def getArgs():
+	parser = argparse.ArgumentParser(description='A (not so) simple helper tool to manage your dotfiles.')
+
+	rootSubparser = parser.add_subparsers(required=True)
+	
+	syncParser = rootSubparser.add_parser('sync', aliases=['s'])
+	syncParser.set_defaults(func=sync)
+
+	syncParser.add_argument('-f', dest='files', required=False, help='File to sync (defaults to all files in the repo)')
+
+	# syncArgs = syncParser.parse_args()
+	args = parser.parse_args('s'.split())
+	return args
+
+
+
+def main():
+	init()
+	a = getArgs()
+	# execute the default function associated with the subparser
+	a.func(a)
+
+
+if __name__ == '__main__':
+	main()
