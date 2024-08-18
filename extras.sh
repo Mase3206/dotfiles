e18f6ddf621bff920ded00f837de12f435ef4c53
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-08-17 22:28:22 -0600

    basic subcommand parsing

--- extras.sh
+++ extras.sh
@@ -7,66 +7,67 @@
 # set -x
 
 
+# --------------------------------------
 # DETECT OS AND DISTRO
 
 function detect_os () {
 	case $OSTYPE in 
 		darwin*)
-			OS_FAMILY="macos"
+			DOTFILES_OS_FAMILY="macos"
 			detect_pkg_manager
 			;;
 		
 		linux*)
-			OS_FAMILY="linux"
+			DOTFILES_OS_FAMILY="linux"
 			detect_pkg_manager
 			;;
 		
 		freebsd*)
-			OS_FAMILY="freebsd"
+			DOTFILES_OS_FAMILY="freebsd"
 			detect_pkg_manager
 			;;
 		
 		*)
 			echo "OS could not be automatically detected."
-			OS_FAMILY="unknown"
+			DOTFILES_OS_FAMILY="unknown"
 			manual_set_os
 
 			detect_pkg_manager
-			if [[ $OS_PKG_MANAGER == "unknown" ]]; then
+			if [[ $DOTFILES_PKG_MANAGER == "unknown" ]]; then
 				echo "Package manager could not be automatically detected."
 				manual_set_pkg_manager
 			fi
 	esac
 
-	echo "Detected OS family: $OS_FAMILY"
+	echo "Detected OS family: $DOTFILES_OS_FAMILY"
 }
 
 
 function detect_pkg_manager () {
 	# debian
 	if [ -x "$(command -v apt-get)" ]; then
-		OS_PKG_MANAGER="apt-get"
+		DOTFILES_PKG_MANAGER="apt-get"
 	# rhel
 	elif [ -x "$(command -v dnf)" ]; then
-		OS_PKG_MANAGER="dnf"
+		DOTFILES_PKG_MANAGER="dnf"
 	# suse
 	elif [ -x "$(command -v zypper)" ]; then
-		OS_PKG_MANAGER="zypper"
+		DOTFILES_PKG_MANAGER="zypper"
 	# freebsd
 	elif [ -x "$(command -v pkg)" ]; then
-		OS_PKG_MANAGER="pkg"
+		DOTFILES_PKG_MANAGER="pkg"
 	# macos
 	elif [ -x "$(command -v brew)" ]; then
-		OS_PKG_MANAGER="brew"
+		DOTFILES_PKG_MANAGER="brew"
 	# unknown
 	else
 		echo "Package manager not found." >&2
-		OS_PKG_MANAGER="unknown"
+		DOTFILES_PKG_MANAGER="unknown"
 		
 		manual_set_pkg_manager
 	fi
 
-	[[ $OS_PKG_MANAGER != "unknown" ]] && echo "Detected package manager: $OS_PKG_MANAGER"
+	[[ $DOTFILES_PKG_MANAGER != "unknown" ]] && echo "Detected package manager: $DOTFILES_PKG_MANAGER"
 }
 
 
@@ -77,7 +78,7 @@ function manual_set_os () {
 	select os in "${os_options[@]}"; do
 		# Test if selected option is even within range
 		if [[ "$REPLY" -gt 0 ]] && [[ "$REPLY" -le "${#os_options[@]}" ]]; then
-			OS_FAMILY=$os
+			DOTFILES_OS_FAMILY=$os
 			break
 		else
 			echo "Unknown option $REPLY. Please choose an option between 1 and ${#os_options[@]}."
@@ -97,7 +98,7 @@ function manual_set_pkg_manager () {
 
 			# Test if selected package manager is installed. Display error and re-prompt if not.
 			if [ -x "$(command -v $pkg_mgr)" ]; then
-				OS_PKG_MANAGER=$pkg_mgr
+				DOTFILES_PKG_MANAGER=$pkg_mgr
 				break
 			else
 				echo "Selected package manager '$pkg_mgr' is not installed."
@@ -112,13 +113,105 @@ function manual_set_pkg_manager () {
 
 
 
+# --------------------------------------
+# SCRIPT INITIALIZATION
+# SUBCOMMAND PARSING
+# GLOBAL HELP
+
 function init () {
 	detect_os
 	
-	if [[ $OS_FAMILY == "unknown" ]] || [[ $OS_PKG_MANAGER == "unknown" ]]; then
+	if [[ $DOTFILES_OS_FAMILY == "unknown" ]] || [[ $DOTFILES_PKG_MANAGER == "unknown" ]]; then
 		echo "OS family and package manager could not be determined. This script will now exit."
 		exit 1
 	fi
 }
+
+
+function parse_subcommand () {
+	case $1 in 
+		zsh)
+			zsh $2 $3 $4
+			;;
+
+		oh-my-zsh | oh_my_zsh | omz)
+			oh_my_zsh $2 $3 $4
+			;;
+
+		*)
+			echo "Missing required parameter"
+			do_help
+			exit 1
+	
+	esac
+		
+}
+
+
+
+# --------------------------------------
+# PACKAGE/APP-SPECIFIC STUFF
+
+function zsh {
+	local option
+	option=$1
+
+	case $option in 
+		detect)
+			if [ -x "$(command -v zsh)" ]; then 
+				echo "Zsh is installed." 
+				DOTFILES_ZSH_INSTALLED=1
+			else
+				echo "Zsh is NOT installed."
+				DOTFILES_ZSH_INSTALLED=0
+			fi
+			;;
+		
+		install)
+			$DOTFILES_PKG_MANAGER install -y zsh
+	
+	esac
+}
+
+function oh_my_zsh {
+	local option
+	option=$1
+
+	case $option in
+		detect) 
+			if [ -d ~/.oh-my-zsh ]; then
+				echo "Oh My Zsh is installed."
+				DOTFILES_OMZ_INSTALLED=1
+			else
+				echo "Oh My Zsh is NOT installed."
+				DOTFILES_OMZ_INSTALLED=0
+			fi
+			;;
+
+		install)
+			CHSH='yes' RUNZSH='no' KEEP_ZSHRC='yes'
+		
+	esac
+}
+
+
+
+
+# -------------------------------------
+# MAIN LOOP
+
+# check global options
+case $1 in
+	-h | help) 
+		# display help
+		do_help
+		;;
+
+	*)
+		# if nothing here matches, just send it all to parse_subcommand
+		parse_subcommand $1 $2 $3
+		;;
+
+esac
 
 init
