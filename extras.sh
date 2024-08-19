923f80aa65d0cf6a620e15c21aa9190cb4ecfb55
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-08-18 18:09:07 -0600

    bash doesn't like nested functions, apparently

--- extras.sh
+++ extras.sh
@@ -190,47 +190,49 @@ function zsh {
 	esac
 
 
-	function zsh_detect () {
-		if [ -x "$(command -v zsh)" ]; then 
-			echo "Zsh is installed." 
-			DOTFILES_ZSH_INSTALLED=1
-		else
-			echo "Zsh is NOT installed."
-			DOTFILES_ZSH_INSTALLED=0
-		fi
-	}
+}
 
 
-	function zsh_install () {
-		# test if Zsh is already installed
-		zsh_detect
-		if [[ $DOTFILES_ZSH_INSTALLED == 1 ]]; then
-			echo "Zsh is already installed!"
-			exit 2
-		fi
-
-		$DOTFILES_PKG_MANAGER install -y zsh
-		# back up existing .zshrc before syncing it with repo
-		mv ~/.zshrc ~/.zshrc.dotbak
-		$DOTFILES_DIR/quicksync.sh sync .zshrc
-	}
+function zsh_detect () {
+	if [ -x "$(command -v zsh)" ]; then 
+		echo "Zsh is installed." 
+		DOTFILES_ZSH_INSTALLED=1
+	else
+		echo "Zsh is NOT installed."
+		DOTFILES_ZSH_INSTALLED=0
+	fi
+}
 
 
-	function zsh_help () {
-		cat << EOF
+function zsh_install () {
+	# test if Zsh is already installed
+	zsh_detect
+	if [[ $DOTFILES_ZSH_INSTALLED == 1 ]]; then
+		echo "Zsh is already installed!"
+		exit 2
+	fi
+
+	$DOTFILES_PKG_MANAGER install -y zsh
+	# back up existing .zshrc before syncing it with repo
+	mv ~/.zshrc ~/.zshrc.dotbak
+	$DOTFILES_DIR/quicksync.sh sync .zshrc
+}
+
+
+function zsh_help () {
+	cat << EOF
 $SHELL_SCRIPT_FILE_NAME zsh [-h] <command>
 
 Commands:
-	\`detect\`: Detect existing install and display its status
-	\`install\`: Install Zsh using detected package manager
+\`detect\`: Detect existing install and display its status
+\`install\`: Install Zsh using detected package manager
 EOF
-		[[ "$1" == "-h" ]] && exit 0 || exit 1
-	}
+	[[ "$1" == "-h" ]] && exit 0 || exit 1
 }
 
-function oh_my_zsh {
-	local tempfold curdir
 
+
+function oh_my_zsh {
 	case $1 in
 		detect) 
 			omz_detect
@@ -247,56 +249,60 @@ function oh_my_zsh {
 	esac
 
 
-	function omz_detect () {
-		if [ -d ~/.oh-my-zsh ]; then
-			echo "Oh My Zsh is installed."
-			DOTFILES_OMZ_INSTALLED=1
-		else
-			echo "Oh My Zsh is NOT installed."
-			DOTFILES_OMZ_INSTALLED=0
-		fi
-	}
+}
 
 
-	function omz_install () {
-		omz_detect
-		if [[ $DOTFILES_OMZ_INSTALLED == 1 ]]; then
-			echo "Oh My Zsh is already installed!"
-			exit 2
-		fi
-
-		# prep folders
-		tempfold=$(mktemp -d)
-		curdir=$(pwd)
-
-		# download install script
-		cd $tempfold
-		curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh > install.sh
-
-		# run with these specific environment variables
-		# CHSH='yes' - tells install.sh to set Zsh as the default shell for this user
-		# RUNZSH='no' - tells install.sh not to run Zsh after the install
-		# KEEP_ZSHRC='yes' - tells install.sh not to create a backup of the existing .zshrc file
-		CHSH='yes' RUNZSH='no' KEEP_ZSHRC='yes' sh install.sh
-
-		rm install.sh
-		cd $curdir
-
-		# automatically sync Terse OMZ theme from repo
-		$DOTFILES_DIR/quicksync.sh ln .oh-my-zsh/themes/terse.zsh-theme
-	}
+function omz_detect () {
+	if [ -d ~/.oh-my-zsh ]; then
+		echo "Oh My Zsh is installed."
+		DOTFILES_OMZ_INSTALLED=1
+	else
+		echo "Oh My Zsh is NOT installed."
+		DOTFILES_OMZ_INSTALLED=0
+	fi
+}
 
 
-	function omz_help () {
-		cat << EOF
+function omz_install () {
+	local tempfold curdir
+
+	omz_detect
+	if [[ $DOTFILES_OMZ_INSTALLED == 1 ]]; then
+		echo "Oh My Zsh is already installed!"
+		exit 2
+	fi
+
+	# prep folders
+	tempfold=$(mktemp -d)
+	curdir=$(pwd)
+
+	# download install script
+	cd $tempfold
+	curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh > install.sh
+
+	# run with these specific environment variables
+	# CHSH='yes' - tells install.sh to set Zsh as the default shell for this user
+	# RUNZSH='no' - tells install.sh not to run Zsh after the install
+	# KEEP_ZSHRC='yes' - tells install.sh not to create a backup of the existing .zshrc file
+	CHSH='yes' RUNZSH='no' KEEP_ZSHRC='yes' sh install.sh
+
+	rm install.sh
+	cd $curdir
+
+	# automatically sync Terse OMZ theme from repo
+	$DOTFILES_DIR/quicksync.sh ln .oh-my-zsh/themes/terse.zsh-theme
+}
+
+
+function omz_help () {
+	cat << EOF
 $SHELL_SCRIPT_FILE_NAME omz [-h] <command>
 
 Commands:
-	\`detect\`: Detect existing install and display its status
-	\`install\`: Install Oh My Zsh
+\`detect\`: Detect existing install and display its status
+\`install\`: Install Oh My Zsh
 EOF
-		[[ "$1" == "-h" ]] && exit 0 || exit 1
-	}
+	[[ "$1" == "-h" ]] && exit 0 || exit 1
 }
 
 
