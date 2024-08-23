95390de783105a0758cd73d851957db5fdb20aa9
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-08-23 10:40:31 -0600

    assume pwd if $DOTFILES_DIR is unset when running install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+
+# If $DOTFILES_DIR is unset, assume current directory
+if [ -z $DOTFILES_DIR ]; then
+	echo "\$DOTFILES_DIR is not set. Assuming current directory: $(pwd)"
+	export DOTFILES_DIR=$(pwd)
+
+	# add to ~/.aliases so it's sourced
+	echo "export DOTFILES_DIR=$DOTFILES_DIR" >> ~/.aliases
+fi
